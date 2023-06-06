a1f85759dd344f41bb23e8b0d1a2feb9ae651583
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2023-06-06 22:05:28 +0530

    style: signup button padding increase

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -206,7 +206,7 @@ export default function Signup() {
         <div className="flex justify-between items-center">
           <input
             type={show.cnfPass ? 'text' : 'password'}
-            id="password"
+            id="cnfPassword"
             name="confirmPassword"
             value={formDetails.confirmPassword}
             placeholder="*******"
@@ -226,13 +226,13 @@ export default function Signup() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="w-6 h-6 "
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
                 />
               </svg>
@@ -262,7 +262,7 @@ export default function Signup() {
         <p className="text-red-500 font-bold capitalize">{errorMsg}</p>
         <button
           className="bg-pink-600 text-white
-         py-1 px-4 w-full "
+         py-2 px-4 w-full "
           type="submit"
         >
           Create New Account
